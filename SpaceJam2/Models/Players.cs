31cf6750b1ae13f0549fb76493929ba31548eefb
seanpkelly <yllekpnaes@gmail.com>
2020-10-12 14:32:01 -0400

    adding identity and such

---
+++ SpaceJam2/Models/Players.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceJam2.Models
+{
+    //public class Rootobject
+    //{
+    //    public Player[] data { get; set; }
+    //    public Meta meta { get; set; }
+    //}
+    public class Meta
+    {
+        public int total_pages { get; set; }
+        public int current_page { get; set; }
+        public int next_page { get; set; }
+        public int per_page { get; set; }
+        public int total_count { get; set; }
+    }
+    public class Players
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string position { get; set; }
+        //public int height_feet { get; set; }
+        //public int height_inches { get; set; }
+        //public int weight_pounds { get; set; }
+        public Team team { get; set; }
+    }
+    public class Team
+    {
+        public int id { get; set; }
+        public string abbreviation { get; set; }
+        public string city { get; set; }
+        public string conference { get; set; }
+        public string division { get; set; }
+        public string full_name { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ SpaceJam2/Models/Stats.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceJam2.Models
+{
+    public class Stats
+    {
+
+        //public class Stats
+        //{
+        //    public datum[] data { get; set; }
+        //}
+
+            public int games_played { get; set; }
+            public int player_id { get; set; }
+            public int season { get; set; }
+            public string min { get; set; }
+            public float fgm { get; set; }
+            public float fga { get; set; }
+            public float fg3m { get; set; }
+            public float fg3a { get; set; }
+            public float ftm { get; set; }
+            public float fta { get; set; }
+            public float oreb { get; set; }
+            public float dreb { get; set; }
+            public float reb { get; set; }
+            public float ast { get; set; }
+            public float stl { get; set; }
+            public float blk { get; set; }
+            public float turnover { get; set; }
+            public float pf { get; set; }
+            public float pts { get; set; }
+            public float fg_pct { get; set; }
+            public float fg3_pct { get; set; }
+            public float ft_pct { get; set; }
+
+            public Stats() { }
+        
+    }
+}
