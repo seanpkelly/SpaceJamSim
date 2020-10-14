6eb40edd2d8eba6e17e3d3cc65a3bb24692c51e1
JordanHallam <jghals12@gmail.com>
2020-10-14 15:49:41 -0400

    starting game controller

---
+++ SpaceJam2/Controllers/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SpaceJam2.Models;
+
+namespace SpaceJam2.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly SpaceJamDAL _SpaceJamDAL;
+        
+        private readonly SpaceJamContext _context;
+
+
+        public GameController(SpaceJamContext context)
+        {
+            
+            _SpaceJamDAL = new SpaceJamDAL();
+            _context = context;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
